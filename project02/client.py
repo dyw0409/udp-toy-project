d46ec0243950b9d5460435f9d1fee115be0d4b47
dyw0409 <dy_woo0409@naver.com>
2025-05-26 21:19:14 +0900

    Update client.py

--- project02/client.py
+++ project02/client.py
@@ -1,14 +1,14 @@
 import socket
 c = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_addr = ('localhost', 3000)
-filename = 'test.jpeg'
+server_addr = ('192.168.5.128', 3000)
+filename = 'test2.jpg'
 #파일명을 보냄
 c.sendto(filename.encode(), server_addr)
 
-with open("받은_파일명" + filename, 'wb') as f:
+with open("0526" + filename, 'wb') as f:
     while True:
         data, addr = c.recvfrom(1024)
-        if not data:
+        if not data == b'__END__':
             break
         f.write(data)
 
